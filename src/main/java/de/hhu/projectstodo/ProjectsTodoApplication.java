fe57f8e5ff2b798c3de8fe68c7e7c7f61928625b
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-28 12:55:44 +0100

    initial commit

---
+++ src/main/java/de/hhu/projectstodo/ProjectsTodoApplication.java
@@ -0,0 +1,13 @@
+package de.hhu.projectstodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectsTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/hhu/projectstodo/ProjectsTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.projectstodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
