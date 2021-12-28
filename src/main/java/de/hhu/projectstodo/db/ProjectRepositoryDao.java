c7f667ccf5e0b74f18208b46c5ef3f92fcaf9d5c
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-28 17:30:14 +0100

    make findAll-method return list of Projects bzw. list of Unternehmen

--- src/main/java/de/hhu/projectstodo/db/ProjectRepositoryDao.java
+++ src/main/java/de/hhu/projectstodo/db/ProjectRepositoryDao.java
@@ -3,5 +3,9 @@ package de.hhu.projectstodo.db;
 import de.hhu.projectstodo.domain.Project;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProjectRepositoryDao extends CrudRepository<Project,Integer> {
+
+    List<Project> findAll();
 }
--- src/main/java/de/hhu/projectstodo/db/UnternehmenRepositoryDao.java
+++ src/main/java/de/hhu/projectstodo/db/UnternehmenRepositoryDao.java
@@ -3,5 +3,9 @@ package de.hhu.projectstodo.db;
 import de.hhu.projectstodo.domain.Unternehmen;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UnternehmenRepositoryDao extends CrudRepository<Unternehmen,Integer> {
+
+    List<Unternehmen> findAll();
 }
