830ed39d980848657c025e30cf242890050f6509
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-28 17:19:19 +0100

    Repositories implemented

---
+++ src/main/java/de/hhu/projectstodo/db/ProjectRepositoryImpl.java
@@ -0,0 +1,18 @@
+package de.hhu.projectstodo.db;
+
+import de.hhu.projectstodo.domain.Project;
+import de.hhu.projectstodo.repositories.ProjectRepository;
+
+import java.util.List;
+
+public class ProjectRepositoryImpl implements ProjectRepository {
+    @Override
+    public List<Project> getAllProjects() {
+        return null;
+    }
+
+    @Override
+    public void saveProject(Project project) {
+
+    }
+}
---
+++ src/main/java/de/hhu/projectstodo/db/UnternehmenRepositoryImpl.java
@@ -0,0 +1,18 @@
+package de.hhu.projectstodo.db;
+
+import de.hhu.projectstodo.domain.Unternehmen;
+import de.hhu.projectstodo.repositories.UnternehmenRepository;
+
+import java.util.List;
+
+public class UnternehmenRepositoryImpl implements UnternehmenRepository {
+    @Override
+    public List<Unternehmen> getAllUnternehmen() {
+        return null;
+    }
+
+    @Override
+    public void saveUnternehmen(Unternehmen unternehmen) {
+
+    }
+}
