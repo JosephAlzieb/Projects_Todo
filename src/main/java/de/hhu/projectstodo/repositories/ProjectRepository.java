814d11db95c54fe580ed004ed694df14218f05d0
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-28 17:24:20 +0100

    add some methods to the Repo

--- src/main/java/de/hhu/projectstodo/repositories/ProjectRepository.java
+++ src/main/java/de/hhu/projectstodo/repositories/ProjectRepository.java
@@ -1,10 +1,10 @@
 package de.hhu.projectstodo.repositories;
 
 import de.hhu.projectstodo.domain.Project;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface ProjectRepository extends CrudRepository<Long, Project> {
-    List<Project> findAllById(Long id);
-}
+public interface ProjectRepository {
+    List<Project> getAllProjects();
+    void saveProject (Project project);
+    }
