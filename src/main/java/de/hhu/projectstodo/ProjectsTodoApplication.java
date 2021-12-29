4114c8ac9a2979e760c18a728ea7199452903604
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-29 13:19:40 +0100

    add flyway repair

--- src/main/java/de/hhu/projectstodo/ProjectsTodoApplication.java
+++ src/main/java/de/hhu/projectstodo/ProjectsTodoApplication.java
@@ -2,6 +2,8 @@ package de.hhu.projectstodo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjectsTodoApplication {
@@ -10,4 +12,12 @@ public class ProjectsTodoApplication {
         SpringApplication.run(ProjectsTodoApplication.class, args);
     }
 
+
+    @Bean
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
 }
