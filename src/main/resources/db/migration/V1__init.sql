d87b427f74237dbed5aaf2d4ca260141d0d0a3a5
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-29 13:20:27 +0100

    create two tables unternehmen and project

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,23 @@
+create table unternehmen
+(
+    id int auto_increment,
+    name text,
+    vertreter text,
+    constraint unternehmen_pk
+        primary key (id)
+);
+
+
+create table project
+(
+    id int auto_increment,
+    title text ,
+    typ text ,
+    color text,
+    description text ,
+    days int ,
+    price decimal ,
+    launchDate datetime ,
+    constraint project_pk
+        primary key (id)
+);
