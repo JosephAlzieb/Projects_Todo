279055216b041de55e619d7c1a9622e61d1a89d0
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-29 17:58:43 +0100

    add some dummy data for testing

---
+++ src/main/resources/db/migration/V3__dummyData.sql
@@ -0,0 +1,24 @@
+insert into project (title, typ, color, description, days, price, launchDate)
+values ('p1','t1','red','d1',5,1000,'2022-12-03 12:00');
+
+insert into project (title, typ, color, description, days, price, launchDate)
+values ('p2','t2','red','d2',5,1000,'2021-09-03 12:00');
+
+insert into project (title, typ, color, description, days, price, launchDate)
+values ('p3','t3','blue','d3',100,10000,'2022-11-03 12:00');
+
+insert into project (title, typ, color, description, days, price, launchDate)
+values ('p4','t4','red','d1',355,1000,'2023-08-03 12:00');
+
+
+insert into unternehmen (name, vertreter)
+values ('u1','Joseph');
+
+insert into unternehmen (name, vertreter)
+values ('u2','Janik');
+
+insert into unternehmen (name, vertreter)
+values ('u3','Joe');
+
+insert into unternehmen (name, vertreter)
+values ('u4','Jan');
