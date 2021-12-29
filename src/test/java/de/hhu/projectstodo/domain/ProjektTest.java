83fd2f136a8f701c7c7bb378f867e5d172b2a22b
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-29 17:02:44 +0100

    create initial tests

---
+++ src/test/java/de/hhu/projectstodo/domain/ProjektTest.java
@@ -0,0 +1,19 @@
+package de.hhu.projectstodo.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProjektTest {
+
+    @Test
+    @DisplayName("Object erstellen")
+    void test_1(){
+        LocalDateTime d1 = LocalDateTime.of(2021, 1, 3, 1, 12);
+        Project project= new Project("p1","any","red","any",5,10000, d1);
+        assertThat(project).isNotNull();
+    }
+}
