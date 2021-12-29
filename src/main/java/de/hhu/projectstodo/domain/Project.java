f39fccc44a6a7ec4702bec7cc1183539dbf393ef
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-29 14:56:05 +0100

    remove all annotation like Entity ,..

--- src/main/java/de/hhu/projectstodo/domain/Project.java
+++ src/main/java/de/hhu/projectstodo/domain/Project.java
@@ -2,18 +2,20 @@ package de.hhu.projectstodo.domain;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-@Entity(name = "Project")
 public class Project {
 
     @Id
     private Integer id;
     private String title;
-    private String type;
+    private String typ;
     private String color;
     private String description;
     private int days;
@@ -22,8 +24,99 @@ public class Project {
     private LocalDateTime launchDate;
     private Set<UnternehmenRef> unternehmen = new HashSet<>();
 
+    void addUnternehmen(Unternehmen un){
+        unternehmen.add(new UnternehmenRef(un.id()));
+    }
+
+    public Project(String title, String typ, String color, String description, int days, double price, LocalDateTime launchDate) {
+        this.title = title;
+        this.typ = typ;
+        this.color = color;
+        this.description = description;
+        this.days = days;
+        this.price = price;
+        this.launchDate = launchDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTyp() {
+        return typ;
+    }
+
+    public void setTyp(String typ) {
+        this.typ = typ;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(LocalDateTime launchDate) {
+        this.launchDate = launchDate;
+    }
 
     public Project() {
     }
 
+    @Override
+    public String toString() {
+        return "Project{" +
+               "id=" + id +
+               ", title='" + title + '\'' +
+               ", typ='" + typ + '\'' +
+               ", color='" + color + '\'' +
+               ", description='" + description + '\'' +
+               ", days=" + days +
+               ", price=" + price +
+               ", launchDate=" + launchDate +
+               ", unternehmen=" + unternehmen +
+               '}';
+    }
 }
--- src/main/java/de/hhu/projectstodo/domain/Unternehmen.java
+++ src/main/java/de/hhu/projectstodo/domain/Unternehmen.java
@@ -1,11 +1,7 @@
 package de.hhu.projectstodo.domain;
 
-import org.springframework.data.relational.core.mapping.Table;
-
-import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity(name="untenehmen")
 public record Unternehmen (@Id Integer id, String name, String vertreter) {
 
     public Unternehmen {
