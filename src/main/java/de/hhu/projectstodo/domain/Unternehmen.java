03f4ebc2752caa7a02e068fc9b796e9c6796d42c
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-30 15:39:40 +0100

    delete the default constructor

--- src/main/java/de/hhu/projectstodo/domain/Unternehmen.java
+++ src/main/java/de/hhu/projectstodo/domain/Unternehmen.java
@@ -3,8 +3,4 @@ package de.hhu.projectstodo.domain;
 import javax.persistence.Id;
 
 public record Unternehmen (@Id Integer id, String name, String vertreter) {
-
-    public Unternehmen {
-    }
-
 }
