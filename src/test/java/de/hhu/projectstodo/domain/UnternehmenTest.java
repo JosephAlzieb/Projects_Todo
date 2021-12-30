3f3cb6d433f79cdeffb971496c9d25a6db9742ee
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-30 12:15:36 +0100

    initial Test for Unternehmen

--- src/test/java/de/hhu/projectstodo/domain/UnternehmenTest.java
+++ src/test/java/de/hhu/projectstodo/domain/UnternehmenTest.java
@@ -1,13 +1,11 @@
 package de.hhu.projectstodo.domain;
 
-import de.hhu.projectstodo.services.UnternehmenService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UnternehmenTest {
-    UnternehmenService service = new UnternehmenService();
 
     @Test
     @DisplayName("Object erstellen")
